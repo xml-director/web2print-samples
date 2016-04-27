eb8647261d90cfd257420bf8293a9e29c29465c6
Andreas Jung <info@zopyx.com>
2016-04-27 13:16:58 +0200

    updated

--- templates/generate-preview.py
+++ templates/generate-preview.py
@@ -4,13 +4,13 @@ import commands
 
 for name in glob.glob('*/*.html'):
     basename, ext = os.path.splitext(name)
+    print basename
 
     cmd = 'pdfreactor.py -i {} -o {}.pdf'.format(name, basename)
-    print cmd
     os.system(cmd)
 
     cmd  = 'convert -density 300 {}.pdf -resize 400 {}.png'.format(basename, basename)
-    print cmd
     os.system(cmd)
 
-
+    cmd  = 'git add {}.png'.format(basename)
+    os.system(cmd)
